4bd1b6142439736c849ee6e670bcc92777557867
python-xiaobo <1486931459@qq.com>
2023-12-18 17:47:59 +0800

    Site updated: 2023-12-18 17:47:59

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -24,7 +24,7 @@ if (cards.length) {
     cards.forEach((card) => {
       card.setAttribute(
         'style',
-        `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 0.9};`
+        `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 0.6};`
       )
     })
   }

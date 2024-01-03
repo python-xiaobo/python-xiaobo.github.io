b2119c5148250b7ff14b4e0feed8aeba3bcb4029
python-xiaobo <1486931459@qq.com>
2024-01-03 21:06:40 +0800

    Site updated: 2024-01-03 21:06:39

--- xhb_js/scrollAnimation.js
+++ xhb_js/scrollAnimation.js
@@ -1,38 +1,5 @@
-const cards = document.querySelectorAll('.index-card')
-if (cards.length) {
-  document.querySelector('.row').setAttribute('style', 'overflow: hidden;')
-  const coefficient = document.documentElement.clientWidth > 768 ? .5 : .3
-  const origin =
-    document.documentElement.clientHeight -
-    cards[0].getBoundingClientRect().height * coefficient
 
-  function throttle(fn, wait) {
-    let timer = null
-    return function () {
-      const context = this
-      const args = arguments
-      if (!timer) {
-        timer = setTimeout(function () {
-          fn.apply(context, args)
-          timer = null
-        }, wait)
-      }
-    }
-  }
 
-  function handle() {
-    cards.forEach((card) => {
-      card.setAttribute(
-        'style',
-        `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 1.5};`/*原来是1:0（动画效果从左边划入）,但是手机端看不见所以调整为1:1.5，降低了动画效果（见网站） */
-      )
-    })
-  }
-
-  document.addEventListener('scroll', throttle(handle, 100))
-}
-
-/*
 
 var ua = navigator.userAgent;
 var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
@@ -66,7 +33,7 @@ if (!isMobile) {
             cards.forEach((card) => {
                 card.setAttribute(
                     'style',
-                    `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 1.5};`
+                    `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 0};`
                 )
             })
         }
@@ -74,4 +41,3 @@ if (!isMobile) {
     }
 }
 
-*/<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>

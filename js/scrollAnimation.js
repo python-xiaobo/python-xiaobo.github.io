26a40ca64a4434da875a33b3364304ee68eac993
python-xiaobo <1486931459@qq.com>
2024-01-03 20:02:28 +0800

    Site updated: 2024-01-03 20:02:26

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -31,3 +31,47 @@ if (cards.length) {
 
   document.addEventListener('scroll', throttle(handle, 100))
 }
+
+/*
+
+var ua = navigator.userAgent;
+var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+    isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+    isAndroid = ua.match(/(Android)\s+([\d.]+)/),
+    isMobile = isIphone || isAndroid;
+
+
+if (!isMobile) {
+    const cards = document.querySelectorAll('.index-card')
+    if (cards.length) {
+        document.querySelector('.row').setAttribute('style', 'overflow: hidden;')
+        const coefficient = document.documentElement.clientWidth > 768 ? .5 : .3
+        const origin =
+            document.documentElement.clientHeight -
+            cards[0].getBoundingClientRect().height * coefficient
+        function throttle(fn, wait) {
+            let timer = null
+            return function () {
+                const context = this
+                const args = arguments
+                if (!timer) {
+                    timer = setTimeout(function () {
+                        fn.apply(context, args)
+                        timer = null
+                    }, wait)
+                }
+            }
+        }
+        function handle() {
+            cards.forEach((card) => {
+                card.setAttribute(
+                    'style',
+                    `--state: ${card.getBoundingClientRect().top - origin < 0 ? 1 : 1.5};`
+                )
+            })
+        }
+        document.addEventListener('scroll', throttle(handle, 100))
+    }
+}
+
+*/<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
